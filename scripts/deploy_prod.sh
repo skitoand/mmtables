8fb9c9e913b904a545c7e8f0f96f34173bad25f3
Андрей <skitovich@MacBook-Air-3.local>
2026-09-14 13:24:23 +0300

    fix: deploy Sfera sync module

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -70,6 +70,7 @@ require_local_file "$ROOT_DIR/styles.css"
 require_local_file "$ROOT_DIR/server.py"
 require_local_file "$ROOT_DIR/sfera_bridge.py"
 require_local_file "$ROOT_DIR/sfera_sso.py"
+require_local_file "$ROOT_DIR/sfera_sync.py"
 require_local_file "$ROOT_DIR/api_v1.py"
 require_local_file "$ROOT_DIR/mcp_http.py"
 require_local_file "$ROOT_DIR/mcp_oauth.py"
@@ -115,6 +116,7 @@ retry 6 remote_scp "$ROOT_DIR/styles.css" "${USER_NAME}@${HOST}:/tmp/mmtable_sty
 retry 6 remote_scp "$ROOT_DIR/server.py" "${USER_NAME}@${HOST}:/tmp/mmtable_server.py"
 retry 6 remote_scp "$ROOT_DIR/sfera_bridge.py" "${USER_NAME}@${HOST}:/tmp/mmtable_sfera_bridge.py"
 retry 6 remote_scp "$ROOT_DIR/sfera_sso.py" "${USER_NAME}@${HOST}:/tmp/mmtable_sfera_sso.py"
+retry 6 remote_scp "$ROOT_DIR/sfera_sync.py" "${USER_NAME}@${HOST}:/tmp/mmtable_sfera_sync.py"
 retry 6 remote_scp "$ROOT_DIR/api_v1.py" "${USER_NAME}@${HOST}:/tmp/mmtable_api_v1.py"
 retry 6 remote_scp "$ROOT_DIR/mcp_http.py" "${USER_NAME}@${HOST}:/tmp/mmtable_mcp_http.py"
 retry 6 remote_scp "$ROOT_DIR/mcp_oauth.py" "${USER_NAME}@${HOST}:/tmp/mmtable_mcp_oauth.py"
@@ -158,6 +160,7 @@ retry 6 remote_ssh "
   install -m 644 /tmp/mmtable_server.py '${REMOTE_APP_DIR}/server.py' &&
   install -m 644 /tmp/mmtable_sfera_bridge.py '${REMOTE_APP_DIR}/sfera_bridge.py' &&
   install -m 644 /tmp/mmtable_sfera_sso.py '${REMOTE_APP_DIR}/sfera_sso.py' &&
+  install -m 644 /tmp/mmtable_sfera_sync.py '${REMOTE_APP_DIR}/sfera_sync.py' &&
   install -m 644 /tmp/mmtable_api_v1.py '${REMOTE_APP_DIR}/api_v1.py' &&
   install -m 644 /tmp/mmtable_mcp_http.py '${REMOTE_APP_DIR}/mcp_http.py' &&
   install -m 644 /tmp/mmtable_mcp_oauth.py '${REMOTE_APP_DIR}/mcp_oauth.py' &&
@@ -203,7 +206,7 @@ remote_ssh "
   echo ---
   curl -I --max-time 10 http://127.0.0.1:4173/ | sed -n '1,10p'
   echo ---
-  stat -c '%y %n' '${REMOTE_APP_DIR}/app.js' '${REMOTE_APP_DIR}/bitrix-chart.js' '${REMOTE_APP_DIR}/index.html' '${REMOTE_APP_DIR}/styles.css' '${REMOTE_APP_DIR}/server.py' '${REMOTE_APP_DIR}/api_v1.py' '${REMOTE_APP_DIR}/mcp_http.py' '${REMOTE_APP_DIR}/mcp_oauth.py'
+  stat -c '%y %n' '${REMOTE_APP_DIR}/app.js' '${REMOTE_APP_DIR}/bitrix-chart.js' '${REMOTE_APP_DIR}/index.html' '${REMOTE_APP_DIR}/styles.css' '${REMOTE_APP_DIR}/server.py' '${REMOTE_APP_DIR}/sfera_sync.py' '${REMOTE_APP_DIR}/api_v1.py' '${REMOTE_APP_DIR}/mcp_http.py' '${REMOTE_APP_DIR}/mcp_oauth.py'
 "
 
 echo "4/4 Checking public URL"
